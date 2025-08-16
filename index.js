12fc0c6e1808bc6ffaf492981f39bc9fe29ab241
nairobianpappy <nairobi.anpappy@gmail.com>
2025-08-16 11:55:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star MALVIN-XD
+Don't Forgot To Give SPACEX-MD a star
 
  Server running on http://localhost:` + PORT)
 })
